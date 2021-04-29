34698206fdc766d17c2a04262b49a2f2e2e27e4e
ftrtz <fabian.treitz@gmail.com>
2021-04-29 16:56:19 +0200

    added confusion matrix

--- Visualisation.py
+++ Visualisation.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
+
 
 
 class Visualisation:
@@ -8,6 +12,18 @@ class Visualisation:
         fig = plt.figure(figsize=(8, 4))
         fig.suptitle(title_text)
         for i in range(10):
+            j = i + np.random.randint(len(y)-10)
             ax = fig.add_subplot(2, 5, i + 1, xticks=[], yticks=[])
-            ax.imshow(np.squeeze(x[i]), cmap='gray')
-            ax.set_title(label=y[i])
+            ax.imshow(np.squeeze(x[j]), cmap='gray')
+            ax.set_title(label=y[j])
+
+    def cm_plot(self, y_test, y_preds, labels):
+        cm = confusion_matrix(y_test, y_preds)
+
+        df_cm = pd.DataFrame(cm, index=labels,
+                             columns=labels)
+        df_cm.index.name = "Actual"
+        df_cm.columns.name = "Predicted"
+        plt.figure(figsize=(8, 6))
+        sn.heatmap(df_cm, cmap="Blues", annot=True, fmt='d')
+        plt.show()
